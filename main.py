cd87e1a438475f7757fe7ef800a4972dd2e49e99
AtharvAwle <atharvawle0156@gmail.com>
2024-01-11 14:38:42 +0530

    basics01

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Hello world!!")
+
+# Variables in PYTHON.
+a = 3 #integer
+b = 5.4  #float
+print(a + b)
+c = True  #Boolean
+name = "atharv awle"  #string
+print(name)
+e = None  #None
+print(e)
+
+
+
+# TYPECASTING IN PYTHON.
+
+a = "5"
+print(int(a) + 5)  #typecasted datatype int()
+
+b = 5
+print(b + 5)
---
+++ operators.py
@@ -0,0 +1,28 @@
+num1 = 10
+num2 = 20
+
+# Arithmetic operators
+
+print("Num1 + Num2 is =", num1 + num2)
+print("Num1 - Num2 is =", num1 - num2)
+print("Num1 * Num2 is =", num1 * num2)
+print("Num1 / Num2 is =", num1 / num2)
+print("Num1 // Num2 is =", num1 // num2)
+print("Num1 modulo Num2 is =", num1 % num2)
+print("Num1 ** num2=", num1**num2)
+
+
+# Assignment operator
+
+a = 4
+# a+=2
+a-=1
+print(a)
+
+
+#comparisn operators
+
+x= 10
+y = 9
+print(x>y)
+print(x<y)
